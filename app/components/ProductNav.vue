9b6e8cfbe8bf9546e685d707cf028a2304d8caa3
paul <paulb896@gmail.com>
2020-01-01 15:08:29 -0800

    Fix nav link

--- app/components/ProductNav.vue
+++ app/components/ProductNav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">Product Info</a>
+    <NLink class="navbar-brand nav-link" to="/">Product Info</NLink>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

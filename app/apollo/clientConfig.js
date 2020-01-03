5c0fdbd6c7e67ac6d7c12539115537a97efa2b14
paul <paulb896@gmail.com>
2020-01-03 05:26:07 -0800

    Only create apollo persisted query link on the client

--- app/apollo/clientConfig.js
+++ app/apollo/clientConfig.js
@@ -3,7 +3,10 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 
 export default function () {
-  const link = createPersistedQueryLink().concat(createHttpLink({ uri: "/graphql", useGETForQueries: true, defaultHttpLink: false }));
+  let link;
+  if (!process.server) {
+    link = createPersistedQueryLink().concat(createHttpLink({ uri: process.env.SERVER_SEARCH_URL, useGETForQueries: true, defaultHttpLink: false }) );
+  }
 
   return {
     httpEndpoint: process.env.SERVER_SEARCH_URL ? process.env.SERVER_SEARCH_URL : 'http://localhost:4000',

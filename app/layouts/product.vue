a200192602c91d9a0c5bf0c4ee938b7b34035438
paul <paulb896@gmail.com>
2020-01-06 07:55:21 -0800

    Move scripts back to head tag due to bug

--- app/layouts/product.vue
+++ app/layouts/product.vue
@@ -4,9 +4,6 @@
     <div class="container-fluid">
       <Nuxt/>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" defer></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" defer></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" defer></script>
   </div>
 </template>
 
--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -29,6 +29,11 @@ export default {
     ],
     link: [
       { rel: "stylesheet", type: "text/css", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" }
+    ],
+    script: [
+      { src: 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' },
+      { src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' }
     ]
   }
 }
